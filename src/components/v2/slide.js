d8f1cfd9cf532288701efd8130463424717b0dbd
Emily Pillay <epillay221@gmail.com>
2021-09-27 11:54:58 -0400

    slide and slide container

---
+++ src/components/v2/slide.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+const Slide = styled.div`
+  height: 100vh;
+  scroll-snap-align: start;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export default Slide
